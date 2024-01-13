2c4bd567d39f997b34e04ff1d7ac272cf074f720
Nxbi <37214@ma-web.nl>
2024-01-13 12:41:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,31 @@ g.lineTo(20, 100);
 g.lineTo(20, 20);
 g.stroke();
 //while (playerDead != true) {
-    
+
 //}
+
+class PlayerController {
+
+    wasd() {
+        let w, a, s, d;
+        if (KeyboardEvent.key == "w"/* && KeyboardEvent.repeat*/) {
+            w = true;
+        } else if (KeyboardEvent.key == "a") {
+            a = true;
+        } else if (KeyboardEvent.key == "s") {
+            s = true;
+        } else if (KeyboardEvent.key == "d") {
+            d = true;
+        } else {
+            w = false;
+
+        }
+
+
+        
+    }
+
+}
 
 class AnimationHandler {
     playerAnimation(xPlayerLocation, yPlayerLocation) {
@@ -31,21 +54,21 @@ class EnitityHandler {
         speed = this.speed;
     }
 
-
-    movement(img, isEnemy, isParticle) {
-        let movingEntity = document.getElementById(img);
-        if (isEnemy == true && isParticle == true) {
-            g.drawImage(img, )
-        }
-        g.drawImage(movingEntity, xLocation, yLocation);
-        
-    }
-
     deathHandler() {
         switch (hp) {
             case 0:
                 playerDead = true;
-                g.drawImage(zerohp, 10, 10)
+                g.drawImage(zerohp, 10, 10);
+                break;
+            case 1:
+                g.drawImage(onehp, 10, 10);
+                break;
+            case 2:
+                g.drawImage(twohp, 10, 10);
+            case 3:
+                g.drawImage(threehp, 10, 10);
+                break;
+
         }
     }
 
@@ -55,15 +78,15 @@ class EnitityHandler {
 }
 
 
-g.fillText("Hello", 10, 100)
+
 class EnemyWaves extends EnitityHandler {
     
     spawner() {
         let enemyAmount = Math.floor(Math.random() * 20);
         for (let i = 0; i > enemyAmount; i++) {
-            this.movement()
+            
         }
     }
 }
-
+g.fillText("Hello", 10, 100);
 g.drawImage(Madman, 10, 10);
